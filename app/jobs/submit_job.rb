dda5491bbfcfd24bd3e3979e889616aceab0d403
Keita Urashima <ursm@ursm.jp>
2023-10-15 20:53:55 +0900

    Refactor

--- app/jobs/submit_job.rb
+++ app/jobs/submit_job.rb
@@ -2,13 +2,14 @@ class SubmitJob < ApplicationJob
   def perform(request)
     DdbjValidator.validate request
 
-    if request.status == 'valid'
-      submission = ActiveRecord::Base.transaction {
-        request.update! status: 'submitted'
-        request.dway_user.submissions.create!(request:)
-      }
+    return unless request.status == 'valid'
 
-      request.write_files to: submission.dir
-    end
+    submission = ActiveRecord::Base.transaction {
+      request.update! status: 'submitted'
+
+      request.dway_user.submissions.create!(request:)
+    }
+
+    request.write_files to: submission.dir
   end
 end
