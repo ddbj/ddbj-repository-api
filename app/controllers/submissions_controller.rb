c85a483031424ce3db9dc1460057b7301caf777d
Keita Urashima <ursm@ursm.jp>
2023-12-08 12:42:41 +0900

    Fix N+1

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,9 +1,15 @@
 class SubmissionsController < ApplicationController
   def index
-    @submissions = dway_user.submissions.includes(request: {objs: :file_blob})
+    @submissions = submissions
   end
 
   def show
-    @submission = dway_user.submissions.find(params[:id].delete_prefix('X-'))
+    @submission = submissions.find(params[:id].delete_prefix('X-'))
+  end
+
+  private
+
+  def submissions
+    dway_user.submissions.includes(request: {objs: :file_blob})
   end
 end
