e2365c870fdc6ed63a494af27b27dd4946a3fa41
Keita Urashima <ursm@ursm.jp>
2024-01-05 19:03:29 +0900

    Fix logout

--- ui/src/routes/+layout.server.ts
+++ ui/src/routes/+layout.server.ts
@@ -1,19 +1,25 @@
-import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
+
+import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ cookies, url }) => {
   const apiKey = cookies.get('apiKey');
 
-  if (new URL(url).pathname !== '/ui/login' && !apiKey) {
+  if (apiKey && apiKey !== 'null') {
+    const res = await fetch('http://localhost:3000/api/me', {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    });
+
+    return {
+      me: await res.json()
+    };
+  }
+
+  if (new URL(url).pathname !== '/ui/login') {
     redirect(307, '/ui/login');
   }
 
-  if (apiKey) {
-    const res = await fetch('http://localhost:3000/api/me', {
-      headers: {
-        Authorization: `Bearer ${cookies.get('apiKey')}`
-      }
-    });
-    return { me: await res.json() };
-  }
+  return {};
 };
